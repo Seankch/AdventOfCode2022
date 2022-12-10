f5d8fe28bae112985333b732fc26c02a6a878b83
Sean Kwek <seankwekthecat@gmail.com>
2022-12-11 03:54:34 +0800

    updated base file

--- Base/q.c
+++ Base/q.c
@@ -1,20 +1,46 @@
 #include <stdio.h>
 #define STRING_BUFFER_SIZE 40
 
-int main(void){
-    FILE *file = fopen("input.txt", "r");
-    if (!file){
+// Function declarations
+int Part_1(void);
+int Part_2(void);
+
+int main(void) {
+    printf("Part 1 value: %d\n", Part_1());
+    printf("Part 2 value: %d\n", Part_2());
+    return 0;
+}
+
+int Part_1(void){
+    FILE* file = fopen("input.txt", "r");
+    if (!file) {
         // If file cannot be opened, return NULL
         return 0;
     }
 
     int val = 0;
-    char buffer[STRING_BUFFER_SIZE] = {'\0'};
-    while (fgets(buffer, STRING_BUFFER_SIZE, file)){
-        
+    char buffer[STRING_BUFFER_SIZE] = { '\0' };
+    while (fgets(buffer, STRING_BUFFER_SIZE, file)) {
+
     }
 
-    printf("%d\n", val);
     fclose(file);
-    return 0;
+    return val;
+}
+
+int Part_2(void) {
+    FILE* file = fopen("input.txt", "r");
+    if (!file) {
+        // If file cannot be opened, return NULL
+        return 0;
+    }
+
+    int val = 0;
+    char buffer[STRING_BUFFER_SIZE] = { '\0' };
+    while (fgets(buffer, STRING_BUFFER_SIZE, file)) {
+
+    }
+
+    fclose(file);
+    return val;
 }
