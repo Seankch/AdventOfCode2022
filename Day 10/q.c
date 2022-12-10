4e7dfa8a93519ac0d3974ed3f7caac005436b600
Sean Kwek <seankwekthecat@gmail.com>
2022-12-11 03:10:26 +0800

    Added comments for day 10

--- Day 10/q.c
+++ Day 10/q.c
@@ -10,25 +10,27 @@ int Part_1(void){
     }
 
     int cycle = 1, value = 0;
-    int sixSignalStrengthSum = 0, signalStrengthSum = 1;
+    int sixSignalStrengthSum = 0, registerValue = 1;
     char buffer[STRING_BUFFER_SIZE] = {'\0'};
     while (fgets(buffer, STRING_BUFFER_SIZE, file)){
         if (sscanf(buffer, "%*s %d", &value) == 1){
-            // addx, increment signal strength at the second cycle
+            // addx, 2 cycles
             for (int i = 0; i < 2; ++i){
+                // If cycle is at 20th, 60th, 100th, 140th, 180th, and 220th, increment sum
                 if ((cycle % 40) == 20){
-                    sixSignalStrengthSum += (cycle * signalStrengthSum);
+                    sixSignalStrengthSum += (cycle * registerValue);
                 }
 
-                signalStrengthSum += ((i == 1)? value : 0);
+                // Increment register value at the second cycle
+                registerValue += ((i == 1)? value : 0);
                 ++cycle;
             }
         }
         else{
+            // If cycle is at 20th, 60th, 100th, 140th, 180th, and 220th, increment sum
             if ((cycle % 40) == 20){
-                sixSignalStrengthSum += (cycle * signalStrengthSum);
+                sixSignalStrengthSum += (cycle * registerValue);
             }
-
             ++cycle;
         }
     }
@@ -45,32 +47,38 @@ void Part_2(void){
     }
 
     char buffer[STRING_BUFFER_SIZE] = {'\0'};
-    int cycle = 1, value = 0, signalStrengthSum = 1;
+    int cycle = 1, value = 0, registerValue = 1;
     while (fgets(buffer, STRING_BUFFER_SIZE, file)){
         if (sscanf(buffer, "%*s %d", &value) == 1){
-            // addx, increment signal strength at the second cycle
+            // addx, 2 cycles
             for (int i = 0; i < 2; ++i){
                 // Check if current printed pixel position is within sprite
-                int isWithinSpriteLeftLimit = ((cycle - 1) % 40) >= signalStrengthSum - 1;
-                int isWithinSpriteRightLimit = ((cycle - 1) % 40) <= signalStrengthSum + 1;
+                // Print '#' if within, else print '.'
+                int isWithinSpriteLeftLimit = ((cycle - 1) % 40) >= registerValue - 1;
+                int isWithinSpriteRightLimit = ((cycle - 1) % 40) <= registerValue + 1;
                 printf("%c", (isWithinSpriteLeftLimit && isWithinSpriteRightLimit)? '#' : '.');
-                signalStrengthSum += (i == 1)? value : 0;
+
+                // Increment signal strength at the second cycle
+                registerValue += (i == 1)? value : 0;
+
+                // If printed pixel position is at the end of row, print a newline
                 if ((cycle % 40) == 0){
                     printf("\n");
                 }
-
                 ++cycle;
             }
         }
         else{
             // Check if current printed pixel position is within sprite
-            int isWithinSpriteLeftLimit = ((cycle - 1) % 40) >= signalStrengthSum - 1;
-            int isWithinSpriteRightLimit = ((cycle - 1) % 40) <= signalStrengthSum + 1;
+            // Print '#' if within, else print '.'
+            int isWithinSpriteLeftLimit = ((cycle - 1) % 40) >= registerValue - 1;
+            int isWithinSpriteRightLimit = ((cycle - 1) % 40) <= registerValue + 1;
             printf("%c", (isWithinSpriteLeftLimit && isWithinSpriteRightLimit)? '#' : '.');
+
+            // If printed pixel position is at the end of row, print a newline
             if ((cycle % 40) == 0){
                 printf("\n");
             }
-
             ++cycle;
         }
     }
