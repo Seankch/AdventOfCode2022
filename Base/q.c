7ae3bf136cb68945cbb2d9676c915f0141dc8a34
Sean Kwek <seankwekthecat@gmail.com>
2022-12-12 00:10:50 +0800

    organized files

--- Base/q.c
+++ Base/q.c
@@ -2,45 +2,37 @@
 #define STRING_BUFFER_SIZE 40
 
 // Function declarations
+void ParseInput(void);
 int Part_1(void);
 int Part_2(void);
 
 int main(void) {
+    ParseInput();
     printf("Part 1 value: %d\n", Part_1());
     printf("Part 2 value: %d\n", Part_2());
     return 0;
 }
 
-int Part_1(void){
+void ParseInput(void) {
     FILE* file = fopen("input.txt", "r");
     if (!file) {
         // If file cannot be opened, return NULL
         return 0;
     }
 
-    int val = 0;
     char buffer[STRING_BUFFER_SIZE] = { '\0' };
     while (fgets(buffer, STRING_BUFFER_SIZE, file)) {
-
     }
 
     fclose(file);
+}
+
+int Part_1(void){
+    int val = 0;
     return val;
 }
 
 int Part_2(void) {
-    FILE* file = fopen("input.txt", "r");
-    if (!file) {
-        // If file cannot be opened, return NULL
-        return 0;
-    }
-
     int val = 0;
-    char buffer[STRING_BUFFER_SIZE] = { '\0' };
-    while (fgets(buffer, STRING_BUFFER_SIZE, file)) {
-
-    }
-
-    fclose(file);
     return val;
 }
